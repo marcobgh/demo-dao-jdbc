284c8d246d2340ff334f8864df81353e35cd82cb
Marco Antonio Borghetti <maborghetti@outlook.com>
2025-02-05 07:36:59 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,15 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
         System.out.println(department);
+        Seller seller = new Seller(21, "bob", "bob@gmain.com", new Date(), 3000.00, department);
+        System.out.println(seller);
     }
 }
