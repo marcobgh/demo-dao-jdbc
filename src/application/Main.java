91b98f72bb90a32d4b3d6826fa9f5f033fac5962
Marco Antonio Borghetti <maborghetti@outlook.com>
2025-02-09 18:17:55 -0300

    Update statement

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
+        /*
         SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println("=== TEST 1: seller findById() ===");
         Seller seller = sellerDao.findById(1);
@@ -28,9 +29,21 @@ public class Main {
         for (Seller s : list) {
             System.out.println(s);
         }
-        System.out.println("\n=== TEST 3: seller insert() ===");
+
+        System.out.println("\n=== TEST 4: seller insert() ===");
         Seller newSeller = new Seller(null, "Greg", "greg@gamil.com", new Date(), 400.00, department);
         sellerDao.insert(newSeller);
         System.out.println("\nInserted! New Id = " + newSeller.getId());
+
+
+        System.out.println("\n=== TEST 5: seller insert() ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
+         */
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        sellerDao.insert(new Seller(null, "Sarah Spencer", "spencersarag@yahoo.com", new Date(), 2100.00, new Department(1, null)));
     }
 }
